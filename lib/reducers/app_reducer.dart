11a19e80971e4e31cac47119b94ac9cc3108b6a5
Daniel Augustin <daniel.augustin1@gmx.de>
2020-06-15 13:46:53 +0200

    Improves Date display in transaction overview

--- lib/reducers/app_reducer.dart
+++ lib/reducers/app_reducer.dart
@@ -1,22 +1,25 @@
+import 'dart:math';
+
 import 'package:financeplanner/actions/actions.dart';
 import 'package:financeplanner/models/app_state.dart';
 import 'package:financeplanner/models/models.dart';
 import 'package:redux/redux.dart';
 
 AppState appReducer(AppState state, dynamic action) {
-  return AppState(
-      transactions: addTransactionReducer(state.transactions, action));
+  return AppState(transactions: addTransactionReducer(state.transactions, action));
 }
 
-final addTransactionReducer =
-    TypedReducer<List<Transaction>, IncrementAction>(_addTransaction);
-List<Transaction> _addTransaction(
-    List<Transaction> transactions, IncrementAction action) {
+final addTransactionReducer = TypedReducer<List<Transaction>, IncrementAction>(_addTransaction);
+List<Transaction> _addTransaction(List<Transaction> transactions, IncrementAction action) {
+  Random rnd = new Random();
+
+  double amount = rnd.nextDouble() * 9999 * 2 - 9999;
+
   return List.from(transactions)
     ..add(new Transaction(
         category: "category",
         description:
             "Paid for some fancy dinner. A crab robbed me but a disguised hero saved me. He swung his yellow umbrella like a champ.",
-        amount: 1000,
-        date: DateTime.now()));
+        amount: amount,
+        dateTime: DateTime.now()));
 }
