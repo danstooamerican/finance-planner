bf046908933d0e96b8b2f7b429e206236353019a
Daniel Augustin <daniel.augustin1@gmx.de>
2020-06-20 16:18:36 +0200

    Fixes overflow errors in detail view and transaction form

--- lib/views/widgets/transaction_form.dart
+++ lib/views/widgets/transaction_form.dart
@@ -92,117 +92,104 @@ class TransactionFormState extends State<TransactionForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            child: TextFormField(
-              controller: _dateController,
-              focusNode: _dateFocus,
-              onTap: () => _selectDate(context),
-              keyboardType: TextInputType.number,
-              readOnly: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Date',
-              ),
-              validator: (text) {
-                if (text == null || text.isEmpty) {
-                  return 'Date is required';
-                }
-                return null;
-              },
-            ),
-            padding: const EdgeInsets.all(8),
-          ),
-          Padding(
-            child: TextFormField(
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              textInputAction: TextInputAction.next,
-              maxLength: 9,
-              focusNode: _amountFocus,
-              onFieldSubmitted: (term) {
-                _fieldFocusChange(context, _amountFocus, _descriptionFocus);
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Amount',
-              ),
-              validator: (text) {
-                if (text == null || text.isEmpty) {
-                  return 'Amount is required';
-                }
-
-                if (!text.isMoney()) {
-                  return "Invalid amount";
-                }
-
-                return null;
-              },
-            ),
-            padding: const EdgeInsets.all(8),
-          ),
-          Padding(
-            child: TextFormField(
-              controller: _descriptionController,
-              textInputAction: TextInputAction.next,
-              focusNode: _descriptionFocus,
-              maxLength: 255,
-              maxLines: null,
-              onFieldSubmitted: (term) {
-                _fieldFocusChange(context, _descriptionFocus, _categoryFocus);
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Description',
-              ),
-              validator: (text) {
-                if (text == null || text.isEmpty) {
-                  return 'Description is required';
-                }
-                return null;
-              },
-            ),
-            padding: const EdgeInsets.all(8),
-          ),
-          Padding(
-            child: TextFormField(
-              controller: _categoryController,
-              textInputAction: TextInputAction.done,
-              focusNode: _categoryFocus,
-              maxLength: 255,
-              onFieldSubmitted: (term) {
-                submitPrimaryAction();
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Category',
-              ),
-              validator: (text) {
-                if (text == null || text.isEmpty) {
-                  return 'Category is required';
-                }
-                return null;
-              },
-            ),
-            padding: const EdgeInsets.all(8),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: new RaisedButton(
-                    onPressed: submitPrimaryAction,
-                    child: Text(widget.primaryActionText),
-                  ),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              child: TextFormField(
+                controller: _dateController,
+                focusNode: _dateFocus,
+                onTap: () => _selectDate(context),
+                keyboardType: TextInputType.number,
+                readOnly: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Date',
                 ),
+                validator: (text) {
+                  if (text == null || text.trim().isEmpty) {
+                    return 'Date is required';
+                  }
+                  return null;
+                },
               ),
-            ],
-          ),
-          if (widget.secondaryAction != null)
+              padding: const EdgeInsets.all(8),
+            ),
+            Padding(
+              child: TextFormField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.next,
+                maxLength: 9,
+                focusNode: _amountFocus,
+                onFieldSubmitted: (term) {
+                  _fieldFocusChange(context, _amountFocus, _descriptionFocus);
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Amount',
+                ),
+                validator: (text) {
+                  if (text == null || text.trim().isEmpty) {
+                    return 'Amount is required';
+                  }
+
+                  if (!text.isMoney()) {
+                    return "Invalid amount";
+                  }
+
+                  return null;
+                },
+              ),
+              padding: const EdgeInsets.all(8),
+            ),
+            Padding(
+              child: TextFormField(
+                controller: _descriptionController,
+                textInputAction: TextInputAction.next,
+                focusNode: _descriptionFocus,
+                maxLength: 255,
+                minLines: 1,
+                maxLines: 5,
+                onFieldSubmitted: (term) {
+                  _fieldFocusChange(context, _descriptionFocus, _categoryFocus);
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Description',
+                ),
+                validator: (text) {
+                  if (text == null || text.trim().isEmpty) {
+                    return 'Description is required';
+                  }
+                  return null;
+                },
+              ),
+              padding: const EdgeInsets.all(8),
+            ),
+            Padding(
+              child: TextFormField(
+                controller: _categoryController,
+                textInputAction: TextInputAction.done,
+                focusNode: _categoryFocus,
+                maxLength: 32,
+                onFieldSubmitted: (term) {
+                  submitPrimaryAction();
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Category',
+                ),
+                validator: (text) {
+                  if (text == null || text.trim().isEmpty) {
+                    return 'Category is required';
+                  }
+                  return null;
+                },
+              ),
+              padding: const EdgeInsets.all(8),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -210,15 +197,31 @@ class TransactionFormState extends State<TransactionForm> {
                   child: Padding(
                     padding: const EdgeInsets.all(8),
                     child: new RaisedButton(
-                      color: Colors.red,
-                      onPressed: submitSecondaryAction,
-                      child: Text(widget.secondaryActionText),
+                      onPressed: submitPrimaryAction,
+                      child: Text(widget.primaryActionText),
                     ),
                   ),
                 ),
               ],
             ),
-        ],
+            if (widget.secondaryAction != null)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: new RaisedButton(
+                        color: Colors.red,
+                        onPressed: submitSecondaryAction,
+                        child: Text(widget.secondaryActionText),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+          ],
+        ),
       ),
     );
   }
