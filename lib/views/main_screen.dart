c74b3a0cc116ab2306d6180c4f3942402ee9399b
Daniel Augustin <daniel.augustin1@gmx.de>
2020-06-22 15:57:21 +0200

    Adds FAB animation

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -5,19 +5,67 @@ import 'package:financeplanner/views/add_transaction_screen.dart';
 import 'package:financeplanner/views/widgets/transaction_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   final Store<AppState> store;
 
   MainScreen({Key key, this.store}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() {
+    return new MainScreenState();
+  }
+}
+
+class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<Offset> _offsetAnimation;
+  ScrollController _scrollController = ScrollController();
+  bool _fabIsVisible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    this._animationController = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    this._offsetAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0, 1.5),
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  MainScreenState() {
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+        if (_fabIsVisible == true && !_animationController.isAnimating) {
+          _animationController.forward();
+          _fabIsVisible = false;
+        }
+      } else {
+        if (_scrollController.position.userScrollDirection == ScrollDirection.forward) {
+          if (_fabIsVisible == false && !_animationController.isAnimating) {
+            _animationController.reverse();
+            _fabIsVisible = true;
+          }
+        }
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: StoreProvider<AppState>(
-        store: store,
+        store: widget.store,
         child: Scaffold(
           body: StoreConnector<AppState, List<Transaction>>(
             converter: (Store<AppState> store) {
@@ -36,29 +84,33 @@ class MainScreen extends StatelessWidget {
             },
             builder: (BuildContext context, List<Transaction> transactions) {
               return TransactionList(
-                store: store,
+                store: widget.store,
                 transactions: transactions,
                 onRefresh: updateTransactionList,
+                scrollController: _scrollController,
               );
             },
           ),
-          floatingActionButton: (FloatingActionButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => AddTransactionScreen(store: store)),
-              );
-            },
-            tooltip: 'Add Transaction',
-            child: Icon(Icons.add),
-          )),
+          floatingActionButton: SlideTransition(
+            position: _offsetAnimation,
+            child: FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddTransactionScreen(store: widget.store)),
+                );
+              },
+              tooltip: 'Add Transaction',
+              child: Icon(Icons.add),
+            ),
+          ),
         ),
       ),
     );
   }
 
   Future<Null> updateTransactionList() async {
-    store.dispatch(fetchTransactions());
+    widget.store.dispatch(fetchTransactions());
 
     return null;
   }
